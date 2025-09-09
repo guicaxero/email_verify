6773e78264f4f4c9c4c0c3adc246db5b4e1e27d4
Guilherme Caxéro <caxeropl@gmail.com>
2025-09-09 18:35:17 -0300

    chore: Configuring eslint for typescript

--- frontend/eslint.config.ts
+++ frontend/eslint.config.ts
@@ -1,29 +1,45 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import tsParser from '@typescript-eslint/parser'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
 
 export default defineConfig([
+  // Ignora a pasta de build
   globalIgnores(['dist']),
+
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
+
+    // Extensões de regras
     extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:react-refresh/recommended'
     ],
+
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      parser: tsParser,
       parserOptions: {
         ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
         sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+        project: './tsconfig.json'
       },
+      globals: globals.browser
     },
+
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off'
     },
-  },
+
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  }
 ])
